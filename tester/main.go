56a92ce2059ec9354938a123b1aa77b96e975896
Kaijie Chen <chen@kaijie.org>
2021-11-18 18:54:34 +0800

    tester

---
+++ tester/main.go
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/kaijchen/tracker/track"
+	"google.golang.org/grpc"
+)
+
+var (
+	addr = "localhost:2333"
+)
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewTrackerClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	switch os.Args[1] {
+	case "r":
+		r, err := c.Report(ctx, &pb.ReportRequest{Key: os.Args[2], Location: os.Args[3]})
+		if err != nil {
+			log.Fatalf("could not report: %v", err)
+		}
+		log.Printf("Result: %v", r.GetOk())
+	case "q":
+		r, err := c.Query(ctx, &pb.QueryRequest{Key: os.Args[2]})
+		if err != nil {
+			log.Fatalf("could not query: %v", err)
+		}
+		log.Printf("Result: %v", r.GetLocation())
+	}
+}
